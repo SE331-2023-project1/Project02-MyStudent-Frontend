95140aa16db7dbf27926e61cd1285248c5577c84
Thanadole Bunyamark <spikeman147@gmail.com>
2023-11-01 14:43:02 +0700

    Fix word

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -171,7 +171,7 @@ function updateComment(this: any) {
                 Update
               </button>
             </form>
-            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ event.reply }}</dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"></dd>
           </div>
         </dl>
       </div>
